e177b88c08025104905966759ea1382ab200815e
kottory <kottory@outlook.com>
2021-07-28 23:38:16 +0800

    first commit

---
+++ find_jiaran.py
@@ -0,0 +1,17 @@
+import requests
+import time
+import json
+
+url = 'http://api.bilibili.com/x/emote/package?business=reply&ids={}'
+
+if __name__ == "__main__":
+    max_id = 300
+    id_list = []
+    for id in range(max_id, 0, -1):
+        re = requests.get(url.format(id))
+        if '嘉然' in re.text:
+            print("Find! in id: {}", id)
+            id_list.append(id)
+        time.sleep(0.13)
+
+    print(id_list)
---
+++ get_emoji.py
@@ -0,0 +1,46 @@
+import requests
+import time
+import json
+import os
+import random
+
+url = 'http://api.bilibili.com/x/emote/package?business=reply&ids={}'
+jiaran_list = [221, 245]
+
+
+if __name__ == "__main__":
+    for pid in jiaran_list:
+        r = requests.get(url.format(pid))
+        data = json.loads(r.text)['data']['packages'][0]
+        emoji_list = data['emote']
+        items = []
+        assets_dir = data['text']
+        for emoji in emoji_list:
+            emoji_name = emoji['meta']['alias']
+            filename = '{}.png'.format(os.path.join(assets_dir, emoji_name))
+            if not os.path.exists(assets_dir):
+                os.makedirs(assets_dir)
+            if not os.path.exists(filename):
+                try:
+                    res = requests.get(emoji['url'])
+                    img = res.content
+                    with open(filename, 'wb') as f:
+                        f.write(img)
+                    items.append(emoji_name)
+                    print("{} Done".format(emoji_name))
+                except Exception as e:
+                    print("{} error!".format(emoji_name))
+
+                time.sleep(0.12)
+            else:
+                items.append(emoji_name)
+
+        info = {
+            "name": assets_dir,
+            "prefix": assets_dir + '_',
+            "type": "png",
+            "icon": random.choice(items),
+            "items": items
+        }
+        with open(os.path.join(assets_dir, "info.json"), 'w') as f:
+            f.write(json.dumps(info, ensure_ascii=False))
